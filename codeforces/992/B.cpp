bc3433e2d190710b391bcfc7546e0a7d5f1325e1
Subhash Padala <111901038@smail.iitpkd.ac.in>
2021-07-20 21:44:00 +0530

    Add solution for problem `B - Nastya Studies Informatics` Link: https://codeforces.com/contest/992/problem/B Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/992/submission/123121207

---
+++ codeforces/992/B.cpp
@@ -0,0 +1,114 @@
+// Problem ID - 992B
+
+//          _____                 ___    __
+//  ___________(_)___   ________ ___ |  / /
+//  __  ___/__  / __ | / /_  __ `/__ | / /
+//  _(__  ) _  /  __ |/ / / /_/ / __ |/ /
+//  /____/  /_/   _____/  \__,_/  _____/
+//  ________________________   ________         ______        ______  ______          _________
+//  ____  _/____  _/___  __/   ___  __ \______ ____  /______ ____  /_____  /________ _______  /
+//   __  /   __  /  __  /      __  /_/ /_  __ `/__  / _  __ `/__  //_/__  //_/_  __ `/_  __  /
+//  __/ /   __/ /   _  /       _  ____/ / /_/ / _  /  / /_/ / _  ,<   _  ,<   / /_/ / / /_/ /
+//  /___/   /___/   /_/        /_/      \__,_/  /_/   \__,_/  /_/|_|  /_/|_|  \__,_/  \__,_/
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef KILL_BUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define IOS ios::sync_with_stdio(false)
+#define TIE cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define pb push_back
+#define int long long
+#define mp make_pair
+#define ln "\n"
+
+#define sz(x) ((int)(x).size())
+#define prec(x) cout << fixed << showpoint; cout << setprecision(x)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vp = vector<pii>;
+using vv = vector<vector<int>>;
+
+const int MOD = (int)1e9 + 7;
+const int MOD2 = (int)998244353;
+const int inf = (int)1e18 + 1;
+
+int powb(int a, int n) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return res;
+}
+
+int powb(int a, int n, int m) {
+    a %= m;
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        n >>= 1;
+    }
+    return res;
+}
+
+int32_t main()
+{
+	    IOS;
+#ifndef KILL_BUG
+    	TIE;
+#endif
+	int l, r, x, y; cin>> l>>r>>x>>y;
+        //a*b = x*y and both a and b should have gcd = x;
+        //gcd(a, b) = x and lcm(a,b) = y;
+        //a*b = x*y; and a is multiple of x and b is multiple ofx;
+        if(y%x!=0)
+        {
+            cout << 0 << ln;
+            return 0;
+        }
+        l = (l+x-1)/x;
+        r = (r/x);
+        y /= x;
+        dbg(l, r, y);
+        int ans = 0;
+        for(int i = l; i*i<=y and i <=r; i++)
+        {
+            if(y%i)
+                continue;
+
+            int a = i;
+            int b = y/i;
+            dbg(a, b);
+            if(a>=l and a<=r and b<=r and b>=l and __gcd(a, b)==1)
+            {
+                if(a==b)
+                    ans+=1;
+                else
+                    ans+=2;
+            }
+        }
+        cout << ans << ln;
+	return 0;
+}
