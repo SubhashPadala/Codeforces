99aaf69dad36aa12fd7b8742d6175708d856373d
Subhash Padala <111901038@smail.iitpkd.ac.in>
2021-07-28 11:15:00 +0530

    Add solution for problem `E - Increasing Frequency` Link: https://codeforces.com/contest/1082/problem/E Tags: binary search, dp, greedy, *2000 Ref: https://codeforces.com/contest/1082/submission/123994829

---
+++ codeforces/1082/E.cpp
@@ -0,0 +1,107 @@
+// Problem ID - 1082E
+
+//          _____                 ___    __
+//  ___________(_)___   ________ ___ |  / /
+//  __  ___/__  / __ | / /_  __ `/__ | / /
+//  _(__  ) _  /  __ |/ / / /_/ / __ |/ /
+//  /____/  /_/   _____/  \__,_/  _____/
+//  ________________________   ________         ______        ______  ______          _________
+//  ____  _/____  _/___  __/   ___  __ \______ ____  /______ ____  /_____  /________ _______  /
+//   __  /   __  /  __  /      __  /_/ /_  __ `/__  / _  __ `/__  //_/__  //_/_  __ `/_  __  /
+//  __/ /   __/ /   _  /       _  ____/ / /_/ / _  /  / /_/ / _  ,<   _  ,<   / /_/ / / /_/ /
+//  /___/   /___/   /_/        /_/      \__,_/  /_/   \__,_/  /_/|_|  /_/|_|  \__,_/  \__,_/
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef KILL_BUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define IOS ios::sync_with_stdio(false)
+#define TIE cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define pb push_back
+#define int long long
+#define mp make_pair
+#define ln "\n"
+
+#define sz(x) ((int)(x).size())
+#define prec(x) cout << fixed << showpoint; cout << setprecision(x)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vp = vector<pii>;
+using vv = vector<vector<int>>;
+
+const int MOD = (int)1e9 + 7;
+const int MOD2 = (int)998244353;
+const int inf = (int)1e18 + 1;
+
+int powb(int a, int n) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return res;
+}
+
+int powb(int a, int n, int m) {
+    a %= m;
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        n >>= 1;
+    }
+    return res;
+}
+
+int32_t main()
+{
+	    IOS;
+#ifndef KILL_BUG
+    	TIE;
+#endif
+	// n, c
+        // choose [l, r] such that f-fc is maximal.
+        // choosing subsegment with maximum sum in O(N) can be done using Kadane's algorithm.
+        int n, c; cin>>n>>c;
+        vi ar(n);
+        for(auto&i: ar)
+            cin>>i;
+
+        vi f(5e5+5);
+        //maximum value of (cnt[1:l-1][c]+cnt[l:i][ar[i]]) for each ar[i]
+        int fc = 0;
+        int ans = 0;
+
+        rep(i, 0, n)
+        {
+            if(ar[i]==c)
+                fc++;
+            else
+                // this optimization is soooo gooooood
+                f[ar[i]] = max(f[ar[i]]+1, fc+1);
+            ans = max(ans, f[ar[i]]-fc);
+            // f[ar[i]]-fc will be number of additional c's we are getting from ar[1:i];
+        }
+        cout << ans + fc << ln;
+	return 0;
+}
