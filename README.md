1619456011617a976205aafe9108d0903910cb72
Subhash Padala <111901038@smail.iitpkd.ac.in>
2021-08-04 20:37:00 +0530

    Add solution for problem `C1 - Potions (Easy Version)` Link: https://codeforces.com/contest/1526/problem/C1 Tags: brute force, data structures, dp, greedy, *1500 Ref: https://codeforces.com/contest/1526/submission/124875088

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [A - Balanced Substring](https://codeforces.com/contest/1569/problem/A) | [GNU C++17](./codeforces/1569/A.cpp) | `implementation` `*800` | Sep/16/2021 20:04 | 
-5 | [C - Carrying Conundrum](https://codeforces.com/contest/1567/problem/C) | [GNU C++17](./codeforces/1567/C.cpp) | `bitmasks` `combinatorics` `dp` `math` `*1600` | Sep/13/2021 21:39 | 
-4 | [B - MEXor Mixup](https://codeforces.com/contest/1567/problem/B) | [GNU C++17](./codeforces/1567/B.cpp) | `bitmasks` `greedy` `*1000` | Sep/13/2021 20:26 | 
-3 | [A - Domino Disaster](https://codeforces.com/contest/1567/problem/A) | [GNU C++17](./codeforces/1567/A.cpp) | `implementation` `strings` `*800` | Sep/13/2021 20:11 | 
-2 | [B1 - Palindrome Game (easy version)](https://codeforces.com/contest/1527/problem/B1) | [GNU C++17](./codeforces/1527/B1.cpp) | `constructive algorithms` `games` `*1200` | Aug/04/2021 20:44 | 
-1 | [C2 - Potions (Hard Version)](https://codeforces.com/contest/1526/problem/C2) | [GNU C++17](./codeforces/1526/C2.cpp) | `data structures` `greedy` `*1600` | Aug/04/2021 20:37 | 
+7 | [A - Balanced Substring](https://codeforces.com/contest/1569/problem/A) | [GNU C++17](./codeforces/1569/A.cpp) | `implementation` `*800` | Sep/16/2021 20:04 | 
+6 | [C - Carrying Conundrum](https://codeforces.com/contest/1567/problem/C) | [GNU C++17](./codeforces/1567/C.cpp) | `bitmasks` `combinatorics` `dp` `math` `*1600` | Sep/13/2021 21:39 | 
+5 | [B - MEXor Mixup](https://codeforces.com/contest/1567/problem/B) | [GNU C++17](./codeforces/1567/B.cpp) | `bitmasks` `greedy` `*1000` | Sep/13/2021 20:26 | 
+4 | [A - Domino Disaster](https://codeforces.com/contest/1567/problem/A) | [GNU C++17](./codeforces/1567/A.cpp) | `implementation` `strings` `*800` | Sep/13/2021 20:11 | 
+3 | [B1 - Palindrome Game (easy version)](https://codeforces.com/contest/1527/problem/B1) | [GNU C++17](./codeforces/1527/B1.cpp) | `constructive algorithms` `games` `*1200` | Aug/04/2021 20:44 | 
+2 | [C2 - Potions (Hard Version)](https://codeforces.com/contest/1526/problem/C2) | [GNU C++17](./codeforces/1526/C2.cpp) | `data structures` `greedy` `*1600` | Aug/04/2021 20:37 | 
+1 | [C1 - Potions (Easy Version)](https://codeforces.com/contest/1526/problem/C1) | [GNU C++17](./codeforces/1526/C1.cpp) | `brute force` `data structures` `dp` `greedy` `*1500` | Aug/04/2021 20:37 | 
---
+++ codeforces/1526/C1.cpp
@@ -0,0 +1,119 @@
+// Problem ID - 1526C1
+
+//          _____                 ___    __
+//  ___________(_)___   ________ ___ |  / /
+//  __  ___/__  / __ | / /_  __ `/__ | / /
+//  _(__  ) _  /  __ |/ / / /_/ / __ |/ /
+//  /____/  /_/   _____/  \__,_/  _____/
+//  ________________________   ________         ______        ______  ______          _________
+//  ____  _/____  _/___  __/   ___  __ \______ ____  /______ ____  /_____  /________ _______  /
+//   __  /   __  /  __  /      __  /_/ /_  __ `/__  / _  __ `/__  //_/__  //_/_  __ `/_  __  /
+//  __/ /   __/ /   _  /       _  ____/ / /_/ / _  /  / /_/ / _  ,<   _  ,<   / /_/ / / /_/ /
+//  /___/   /___/   /_/        /_/      \__,_/  /_/   \__,_/  /_/|_|  /_/|_|  \__,_/  \__,_/
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef KILL_BUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<class Fun>
+class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T>
+    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+
+#define IOS ios::sync_with_stdio(false)
+#define TIE cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define pb push_back
+#define int long long
+#define mp make_pair
+#define ln "\n"
+
+#define sz(x) ((int)(x).size())
+#define prec(x) cout << fixed << showpoint; cout << setprecision(x)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vp = vector<pii>;
+using vv = vector<vector<int>>;
+
+const int MOD = (int)1e9 + 7;
+const int MOD2 = (int)998244353;
+const int inf = (int)1e18 + 1;
+
+int powb(int a, int n) {
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a;
+        a = a * a;
+        n >>= 1;
+    }
+    return res;
+}
+
+int powb(int a, int n, int m) {
+    a %= m;
+    int res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        n >>= 1;
+    }
+    return res;
+}
+
+int32_t main()
+{
+	    IOS;
+#ifndef KILL_BUG
+    	TIE;
+#endif
+	int n; cin>>n;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int sum = 0;
+        rep(i, 0, n)
+        {
+            int a; cin>>a;
+            sum += a;
+            pq.push(a);
+            dbg(pq.top());
+            while(sum <0)
+            {
+                sum -= pq.top();
+                pq.pop();
+            }
+        }
+        cout << pq.size() << ln;
+	return 0;
+}
